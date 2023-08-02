5c4d1304a303d1cfa9b24c492dc1e2baccea749a
Noah Fawwas <noahdzakki12@gmail.com>
2023-08-02 13:43:00 +0700

    nike logo add

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -5,7 +5,7 @@
 
 <nav class="navbar navbar-expand-lg navbar-light bg-info p-3">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">DBook Inc</a>
+        <a class="navbar-brand" href="#"><img src="/gambar/Nike.png" alt="Nike" width="110" height="50"></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
             aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
