23af403f399e95a2bd75e4b6ea972ff142450043
Noah Fawwas <noahdzakki12@gmail.com>
2023-08-11 08:48:03 +0700

    recom

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,7 @@
     <link rel="stylesheet" href="css/style.css">
 </head>
     <body class="Home">
+    <body class="New_Rec">
 
 </body>
 </html>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -17,5 +17,92 @@
             </div>
         </div>
 
+        <div class="New_Rec">
+            <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en"><head>
+                <meta http-equiv="content-type" content="text/html" charset="utf-8">
+                <title>frame_255</title>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <meta name="description" content="">
+                <link rel="StyleSheet" href="frame_255.css">
+                <script src="https://secure.exportkit.com/cdn/js/ek_googlefonts.js?v=6"></script>
+                <!-- Add your custom HEAD content here -->
+        
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Manrope:400,700,800"></head>
+            <body>
+                <div id="content-container">
+                    <div id="_bg__frame_255"></div>
+        
+                    <div id="group_13">
+                        <div id="rectangle_4"></div>
+                        <div id="line_1"></div>
+                        <div id="line_2"></div>
+        
+                        <div id="group_12">
+        
+                            <div id="frame_5">
+        
+                            </div>
+                            <img src="skins/unnamed__2__removebg_preview_1.png" id="unnamed__2__removebg_preview_1">
+        
+                        </div>
+        
+                        <div id="group_8">
+        
+                            <div id="frame_6">
+        
+                            </div>
+                            <img src="skins/unnamed__3__removebg_preview_1.png" id="unnamed__3__removebg_preview_1">
+        
+                        </div>
+        
+                        <div id="group_10">
+        
+                            <div id="frame_4">
+        
+                            </div>
+                            <img src="skins/unnamed_removebg_preview_1.png" id="unnamed_removebg_preview_1">
+        
+                        </div>
+        
+                        <div id="frame_8">
+                            <div id="vtg_v_2_hi_black_ws">
+                                VTG V.2 HI Black WS
+                            </div>
+                            <div id="shop_now__">
+                                Shop Now +
+                            </div>
+        
+                        </div>
+        
+                        <div id="frame_9">
+                            <div id="vtg_courth_hi_natural_navy_gs">
+                                VTG Courth Hi<br>Natur	al Navy GS
+                            </div>
+                            <div id="shop_now___ek1">
+                                Shop Now +
+                            </div>
+        
+                        </div>
+        
+                        <div id="frame_10">
+                            <div id="vulcan_hi_navy_ws">
+                                Vulcan Hi Navy WS
+                            </div>
+                            <div id="shop_now___ek2">
+                                Shop Now +
+                            </div>
+        
+                        </div>
+        
+                    </div>
+        
+                </div>
+            
+        </body></html>
+        </div>
+        
+
 
     @stop
+
+    
